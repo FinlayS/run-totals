ab5af025385a99a389cd8ede8a16002331cfbc20
FinlayS <fsgm01@gmail.com>
2020-08-03 22:40:01 +0100

    tidy getRuns

--- utils/getRuns.js
+++ utils/getRuns.js
@@ -2,15 +2,12 @@ import allRuns from "../data/runs"
 
 export function getRunList() {
   let data = [];
-  let id, description, date
   allRuns.runs.forEach((run) => {
-    id = run.id
-    description = run.description
-    date = run.date
-    data.push({id, description, date})
-    console.log(run.id, run.description, run.date)
-    console.log('id',id, 'desctip', description, 'date', date)
-    console.log("DATA", data)
+    data.push({
+      id:run.id,
+      description:run.description,
+      date:run.date
+    })
   })
   return data
 }

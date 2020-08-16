e8c573a4b287ef916af1e0fee18554a6e0253a82
FinlayS <fsgm01@gmail.com>
2020-08-16 11:57:07 +0100

    create login

--- src/routers/laps.js
+++ src/routers/laps.js
@@ -4,6 +4,7 @@ const Lap = require('../models/lap')
 
 router.post('/laps', async (req, res) => {
   const lap = new Lap(req.body)
+
   try {
     await lap.save()
     res.status(201).send(lap)
@@ -14,11 +15,14 @@ router.post('/laps', async (req, res) => {
 
 router.get('/laps/:id', async (req, res) => {
   const _id = req.params.id
+
   try {
     const lap = await Lap.findById(_id)
+
     if (!lap) {
       return res.status(404).send()
     }
+
     res.send(lap)
   } catch (e) {
     res.status(500).send()
@@ -27,11 +31,13 @@ router.get('/laps/:id', async (req, res) => {
 
 router.get('/laps/:id?', async (req, res) => {
   const query = req.query.runId
+
   try {
     const laps = await Lap.find({runId: query})
     if (laps.length < 1) {
       return res.status(404).send()
     }
+
     res.send(laps)
   } catch (e) {
     res.status(500).send(e)
@@ -42,14 +48,21 @@ router.patch('/laps/:id', async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ["runId", , "runId", "lapNo", "lapTime", "lapDistance"]
   const isValidOperation = updates.every((update ) => allowedUpdates.includes(update))
+
   if (!isValidOperation) {
     return res.status(400).send({ error: "invalid update" })
   }
+
   try {
-    const lap = await Lap.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    const lap = await Lap.findById(req.params.id)
+
+    updates.forEach((update) => lap[update] = req.body[update])
+    await lap.save()
+
     if (!lap) {
       return res.status(404).send()
     }
+
     res.send(lap)
   } catch(e) {
     return res.status(400).send(e)
@@ -59,9 +72,11 @@ router.patch('/laps/:id', async (req, res) => {
 router.delete('/laps/:id', async (req, res) => {
   try {
     const lap = await Lap.findByIdAndDelete(req.params.id)
+
     if (!lap) {
       return res.status(404).send()
     }
+
     res.send(lap)
   } catch(e) {
     res.status(500).send()
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,11 +4,22 @@ const User = require('../models/user')
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body)
+
   try {
     await user.save()
     res.status(201).send(user)
+
   } catch (e) {
     res.status(400).send(e)
+  }
+})
+
+router.post('/users/login', async (req, res) =>{
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    res.send(user)
+  } catch (e) {
+    res.status(400).send()
   }
 })
 
@@ -23,11 +34,14 @@ router.get('/users', async (req, res) => {
 
 router.get('/users/:id', async (req, res) => {
   const _id = req.params.id
+
   try {
     const user = await User.findById(_id)
+
     if (!user) {
       return res.status(404).send()
     }
+
     res.send(user)
   } catch (e) {
     res.status(500).send()
@@ -38,9 +52,11 @@ router.patch('/users/:id', async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ["name", "age", "email", "password"]
   const isValidOperation = updates.every((update ) => allowedUpdates.includes(update))
+
   if (!isValidOperation) {
     return res.status(400).send({ error: "invalid update" })
   }
+
   try {
     const user = await User.findById(req.params.id)
 
@@ -50,6 +66,7 @@ router.patch('/users/:id', async (req, res) => {
     if (!user) {
       return res.status(404).send()
     }
+
     res.send(user)
   } catch(e) {
     return res.status(400).send(e)
@@ -59,9 +76,11 @@ router.patch('/users/:id', async (req, res) => {
 router.delete('/users/:id', async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id)
+
     if (!user) {
       return res.status(404).send()
     }
+
     res.send(user)
   } catch(e) {
     res.status(500).send()

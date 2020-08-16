e4e45bb807327034bb8544491573ab94943e6525
FinlayS <fsgm01@gmail.com>
2020-08-16 16:01:19 +0100

    user authentication done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ const lapRouter = require('./routers/laps')
 
 const app = express()
 const port = process.env.PORT || 3001
+// const serviceUnavailable = process.env.SERVICE_UNAVAILABLE
+
+// app.use((req, res, next) => {
+//  console.log(serviceUnavailable)
+//  if (!serviceUnavailable == true)  {
+//   next()
+//  } else {
+//   res.send('Service unavailable')
+//  }
+// })
+
+// app.use((req, res, next) => {
+//  console.log(req.method, req.path)
+//  next()
+// })
 
 app.use(express.json())
 app.use(userRouter, runRouter, lapRouter)

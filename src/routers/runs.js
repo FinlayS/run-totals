95da96511f8fe060a280a1e21cee8deea1783204
Finlay <FinlayS@users.noreply.github.com>
2020-08-16 16:03:10 +0100

    Authentication (#7)
    * hashing passwords
    
    * create login
    
    * generating auth token on register and login
    
    * user authentication done

--- src/routers/runs.js
+++ src/routers/runs.js
@@ -4,9 +4,11 @@ const Run = require('../models/run')
 
 router.post('/runs', async (req, res) => {
   const run = new Run(req.body)
+
   try {
     await run.save()
     res.status(201).send(run)
+
   } catch (e) {
     res.status(400).send(e)
   }
@@ -16,6 +18,7 @@ router.get('/runs', async (req, res) => {
   try {
     const runs = await Run.find({})
     res.send(runs)
+
   } catch (e) {
     res.status(500).send()
   }
@@ -23,11 +26,14 @@ router.get('/runs', async (req, res) => {
 
 router.get('/runs/:id', async  (req, res) => {
   const _id = req.params.id
+
   try {
     const run = await Run.findById(_id)
+
     if (!run) {
       return res.status(404).send()
     }
+
     res.send(run)
   } catch (e) {
     res.status(500).send()
@@ -38,14 +44,21 @@ router.patch('/runs/:id', async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ["description", , "date"]
   const isValidOperation = updates.every((update ) => allowedUpdates.includes(update))
+
   if (!isValidOperation) {
     return res.status(400).send({ error: "invalid update" })
   }
+
   try {
-    const run = await Run.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    const run = await Run.findById(req.params.id)
+
+    updates.forEach((update) => run[update] = req.body[update])
+    await run.save()
+
     if (!run) {
       return res.status(404).send()
     }
+
     res.send(run)
   } catch(e) {
     return res.status(400).send(e)
@@ -55,9 +68,11 @@ router.patch('/runs/:id', async (req, res) => {
 router.delete('/runs/:id', async (req, res) => {
   try {
     const run = await Run.findByIdAndDelete(req.params.id)
+
     if (!run) {
       return res.status(404).send()
     }
+
     res.send(run)
   } catch(e) {
     res.status(500).send()

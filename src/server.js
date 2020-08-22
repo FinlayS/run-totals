4628b3635333d40497deef6d67029551f171b00e
FinlayS <fsgm01@gmail.com>
2020-08-23 00:52:13 +0100

    removing app_use_path

--- src/server.js
+++ src/server.js
@@ -10,7 +10,6 @@ const lapRouter = require('./routers/laps')
 const app = express()
 const port = process.env.PORT || 3001
 
-app.use('/', express.static(path.join(__dirname, '/client/build')));
 app.use(cors())
 app.use(express.json())
 app.use(userRouter, runRouter, lapRouter)

4288b20fb07c2afd8ce6ef6466dae7d9315da030
FinlayS <fsgm01@gmail.com>
2020-08-23 12:39:08 +0100

    new custom server

--- src/server.js
+++ src/server.js
@@ -1,19 +1,28 @@
-require('dotenv').config()
 const express = require('express')
+const next = require('next')
 require('./db/mongoose')
 const cors = require('cors')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
 const userRouter = require('./routers/user')
 const runRouter = require('./routers/runs')
 const lapRouter = require('./routers/laps')
 
-const app = express()
-const port = process.env.PORT || 3001
+app.prepare().then(() => {
+ const server = express()
 
-app.use(cors())
-app.use(express.json())
-app.use(userRouter, runRouter, lapRouter)
+ server.use(cors())
+ server.use(express.json())
+ server.use(userRouter, runRouter, lapRouter)
+ server.all('*', (req, res) => {
+  return handle(req, res)
+ })
 
-app.listen(port,() => {
- console.log('Server is up on port', + port)
+ server.listen(port,() => {
+  console.log('Server is up on port', + port)
+ })
 })

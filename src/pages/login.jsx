3be4fce8ea0aba9cbb48f9b6fa4c77eb44869da8
FinlayS <fsgm01@gmail.com>
2020-12-13 11:03:11 +0000

    saving work

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+
 import Header from '../components/Header';
+import { validationSchema } from "../validation/login";
 import LoginForm from '../components/account/LoginForm'
+
+const methods = useForm({
+  validationSchema,
+  mode: "onTouched"
+})
 
 const Login = () => {
   return (
@@ -9,7 +17,7 @@ const Login = () => {
       <div>
         <h3 className='login-title'>Log in to existing account</h3>
         <div className='container d-flex align-items-center flex-column'>
-          <LoginForm/>
+          <LoginForm methods={methods}/>
         </div>
       </div>
     </div>

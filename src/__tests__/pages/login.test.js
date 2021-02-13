2138fee290bd7df773b19d045ef00826ff42abd0
FinlayS <fsgm01@gmail.com>
2021-02-13 13:39:46 +0000

    adding jest mock

--- src/__tests__/pages/login.test.js
+++ src/__tests__/pages/login.test.js
@@ -2,15 +2,19 @@ import React from 'react';
 import {act, fireEvent, render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../pages/login';
+import {userLogin} from "../../api/user";
 
-const userLogin = jest.fn();
+jest.mock('../../api/user', () => ({ userLogin: jest.fn() }));
 
-let emailInput, loginPage, passwordInput, submitButton;
+let emailInput, loginPage, passwordInput, loginButton;
+
+const validEmailInput = 'valid@email.com';
+const validPasswordInput = 'validPassw0rd';
 
 const elementContainers = async () => {
   emailInput = screen.getByTestId('email-input');
   passwordInput = screen.getByTestId('password-input');
-  submitButton = screen.getByRole("button", { name: "Login" });
+  loginButton = screen.getByRole('button', { name: 'Login' });
 };
 
 describe('Login page tests', () => {
@@ -23,80 +27,94 @@ describe('Login page tests', () => {
 
   describe('Page Render', () => {
     it('should show email share message', async () => {
-
       expect(screen.getByText("We'll never share your email with anyone else."))
         .toBeInTheDocument();
     })
 
     it('should render elements', async () => {
-
       expect(emailInput).toBeInTheDocument();
       expect(passwordInput).toBeInTheDocument();
-      expect(submitButton).toBeInTheDocument();
+      expect(loginButton).toBeInTheDocument();
     })
 
-    it('continue button is not enabled', async () => {
+    it('should render placeholder text', async () => {
+      expect(screen.getAllByPlaceholderText('Enter email'))
+        .toBeTruthy()
+      expect(screen.getAllByPlaceholderText('Password'))
+        .toBeTruthy()
+    })
 
-      expect(submitButton).toBeDisabled();
+    it('should remder disabled login button', async () => {
+      expect(loginButton).toBeDisabled();
     })
   })
 
   describe('Client side validation', () => {
     it('should show empty email warning', async () => {
-
+      userEvent.type(passwordInput, validPasswordInput)
       await act(async () => fireEvent.blur(emailInput))
       expect(
-        screen.getByText("Please enter an email address")
+        screen.getByText('Please enter an email address')
       ).toBeInTheDocument();
 
-      expect(submitButton).toBeDisabled();
+      expect(loginButton).toBeDisabled();
     })
 
     it('should show invalid email warning', async () => {
-
+      userEvent.type(passwordInput, validPasswordInput)
       userEvent.type(emailInput, 'invalid@email')
       await act(async () => fireEvent.blur(emailInput))
 
       expect(
-        screen.getByText("Please enter a valid email address")
+        screen.getByText('Please enter a valid email address')
       ).toBeInTheDocument();
-      expect(submitButton).toBeDisabled();
+      expect(loginButton).toBeDisabled();
     })
 
     it('should show empty password warning', async () => {
-
+      userEvent.type(emailInput, validEmailInput)
       await act(async () => fireEvent.blur(passwordInput))
       expect(
-        screen.getByText("Please enter a valid password")
+        screen.getByText('Please enter a valid password')
       ).toBeInTheDocument();
 
-      expect(submitButton).toBeDisabled();
+      expect(loginButton).toBeDisabled();
     })
 
     it('should show invalid password warning', async () => {
-
-      userEvent.type(passwordInput, 'invalid@email')
+      userEvent.type(emailInput, validEmailInput)
+      userEvent.type(passwordInput, 'invalidPassword')
       await act(async () => fireEvent.blur(passwordInput))
 
       expect(
-        screen.getByText("Must Contain 8 characters, one uppercase and one number")
+        screen.getByText('Must Contain 8 characters, one uppercase and one number')
       ).toBeInTheDocument();
 
-      expect(submitButton).toBeDisabled();
+      expect(loginButton).toBeDisabled();
     })
 
-    it('can continue with valid input', async () => {
+    it('should enable login button with valid input', async () => {
+      userEvent.type(emailInput, validEmailInput)
+      userEvent.type(passwordInput, validPasswordInput)
 
-      userEvent.type(emailInput, 'valid@email.com')
-      userEvent.type(passwordInput, 'ValidPassw0rd')
-      await act(async () => fireEvent.blur(passwordInput))
-
-      expect(submitButton).toBeEnabled();
+      expect(loginButton).toBeEnabled()
     })
   })
 
   describe('Client side validation', () => {
+    it('should call login function', async () => {
+      userEvent.type(emailInput, validEmailInput)
+      userEvent.type(passwordInput, validPasswordInput)
+      await act(async () => userEvent.click(loginButton))
 
+      expect(userLogin).toBeCalledTimes(1)
+      expect(userLogin).toBeCalledWith(
+        {
+          'email': validEmailInput,
+          'password': validPasswordInput
+        }
+      )
+    })
   })
 
 })  

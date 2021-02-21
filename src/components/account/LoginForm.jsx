b6cb3fad79858d13513f75dcdf97c7ad37f886a3
FinlayS <fsgm01@gmail.com>
2021-02-21 18:26:50 +0000

    adding spinne to reg and login buttons

--- src/components/account/LoginForm.jsx
+++ src/components/account/LoginForm.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useRouter } from 'next/router'
-import { yupResolver } from '@hookform/resolvers/yup';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router"
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import { loginValidation } from "../../validation/login";
-import { userLogin } from '../../api/user';
+import { userLogin } from "../../api/user";
 
 const LoginForm = () => {
   const methods = useForm({
@@ -13,19 +13,20 @@ const LoginForm = () => {
     reValidateMode: "onChange"
   });
 
-  const email = methods.watch('email')
-  const password = methods.watch('password')
-  const hasNoErrors = Object.keys(methods.errors).length === 0;
-  const canContinue = hasNoErrors && email && password;
-  const { register, handleSubmit, errors } = methods;
-
   const router = useRouter()
+  const [loader, setLoader] = useState(false);
   const [state , setState] = useState({
-    email : '',
-    password : '',
+    email : "",
+    password : "",
     successMessage: null,
-    error: ''
+    error: "",
   })
+
+  const email = methods.watch("email")
+  const password = methods.watch("password")
+  const hasNoErrors = Object.keys(methods.errors).length === 0;
+  const canContinue = hasNoErrors && email && password && !loader;
+  const { register, handleSubmit, errors } = methods;
 
   const resetServerError = () => {
     showError(null)
@@ -39,90 +40,96 @@ const LoginForm = () => {
   }
 
   const sendDetailsToServer = async () => {
-    resetServerError()
     try {
       const logInResp = await userLogin({ email, password })
-
       if (logInResp.status === 200) {
-        setState(prevState => ({
-          ...prevState,
-          'successMessage': 'Login successful. Redirecting to home page..'
-        }))
-        await router.push('/runs-main')
+        await router.push("/runs-main")
+        setLoader(false)
       } else {
         if (logInResp.response.status) {
           showError(logInResp.response.data.error.message);
         }
       }
     } catch (e) {
-      showError('Sorry, something went wrong')
+      showError("Sorry, something went wrong")
     }
+    setLoader(false)
   }
 
   const handleSubmitClick = async (e) => {
+    resetServerError()
+    setLoader(true)
     await sendDetailsToServer(e)
   }
 
   const redirectToLogin = async () => {
-    await router.push('/register');
+    await router.push("/register");
   }
 
   return(
-    <div className='card col-12 col-lg-4 login-card mt-2 hv-center'>
+    <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
       <form onSubmit={handleSubmit(handleSubmitClick)}>
-        <div className='form-group text-left'>
-          <label htmlFor='exampleInputEmail1'>Email address</label>
-          <input className='form-control'
-                 data-testid='email-input'
-                 id='email'
-                 name='email'
-                 aria-describedby='emailHelp'
-                 placeholder='Enter email'
+        <div className="form-group text-left">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input className="form-control"
+                 data-testid="email-input"
+                 id="email"
+                 name="email"
+                 aria-describedby="emailHelp"
+                 placeholder="Enter email"
                  onChange={resetServerError}
                  ref={register}
           />
-          <small id='emailHelp' className='form-text text-muted'>We'll never share your email with anyone else.</small>
-          <div className='alert alert-danger mt-2' style={{display: errors.email ? 'block' : 'none' }} role='alert'>
+          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+          <div className="alert alert-danger mt-2" style={{display: errors.email ? "block" : "none" }} role="alert">
             {errors.email && errors.email.message}
           </div>
         </div>
-        <div className='form-group text-left'>
-          <label htmlFor='exampleInputPassword1'>Password</label>
-          <input type='password'
-                 className='form-control'
-                 id='password'
-                 data-testid='password-input'
-                 name='password'
-                 placeholder='Password'
+        <div className="form-group text-left">
+          <label htmlFor="exampleInputPassword1">
+            Password</label>
+          <input type="password"
+                 className="form-control"
+                 id="password"
+                 data-testid="password-input"
+                 name="password"
+                 placeholder="Password"
                  onChange={resetServerError}
                  ref={register}
           />
         </div>
-        <div className='alert alert-danger mt-2' style={{display: errors.password ? 'block' : 'none' }} role='alert'>
+        <div className="alert alert-danger mt-2" style={{display: errors.password ? "block" : "none" }} role="alert">
           {errors.password && errors.password.message}
         </div>
         <button
-          type='submit'
-          className='btn btn-primary'
-          name='login'
+          type="submit"
+          className="btn btn-primary"
+          name="login"
           disabled={!canContinue}
         >
+          { loader && (
+            <div
+              className="loader"
+              data-testid="loader"
+              style={{ marginLeft: "10px" }}
+            />
+          )}
           Login
         </button>
       </form>
-      <div className='alert alert-success mt-2'
-           style={{display: state.successMessage ? 'block' : 'none' }}
-           role='alert'>
+      <div className="alert alert-success mt-2"
+           style={{display: state.successMessage ? "block" : "none" }}
+           role="alert">
         {state.successMessage}
       </div>
-      <div className='alert alert-danger mt-2'
-           style={{display: state.error ? 'block' : 'none' }}
-           role='alert'>
+      <div className="alert alert-danger mt-2"
+           style={{display: state.error ? "block" : "none" }}
+           role="alert">
         {state.error}
       </div>
-      <div className='mt-2'>
+      <div className="mt-2">
         <span>Don't have an account? </span>
-        <span className='loginText' onClick={() => redirectToLogin()}>sign up here</span>
+        <span className="loginText" onClick={() => redirectToLogin()}>sign up here</span>
       </div>
     </div>
   )
--- src/components/account/RegisterForm.jsx
+++ src/components/account/RegisterForm.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useRouter } from 'next/router';
-import { yupResolver } from '@hookform/resolvers/yup';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import { registerValidation } from "../../validation/register";
-import { userRegister } from '../../api/user';
+import { userRegister } from "../../api/user";
 
 const RegisterForm = () => {
   const methods = useForm({
@@ -13,21 +13,22 @@ const RegisterForm = () => {
     reValidateMode: "onChange"
   });
 
-  const email = methods.watch('email')
-  const password = methods.watch('password')
-  const confirmPassword = methods.watch('confirmPassword')
-  const hasNoErrors = Object.keys(methods.errors).length === 0;
-  const canContinue = hasNoErrors && email && password && confirmPassword;
-  const { register, handleSubmit, errors } = methods;
-
   const router = useRouter()
+  const [loader, setLoader] = useState(false);
   const [state , setState] = useState({
-    email : '',
-    password : '',
-    confirmPassword: '',
+    email : "",
+    password : "",
+    confirmPassword: "",
     successMessage: null,
-    error: ''
+    error: ""
   })
+
+  const email = methods.watch("email")
+  const password = methods.watch("password")
+  const confirmPassword = methods.watch("confirmPassword")
+  const hasNoErrors = Object.keys(methods.errors).length === 0;
+  const canContinue = hasNoErrors && email && password && confirmPassword && !loader;
+  const { register, handleSubmit, errors } = methods;
 
   const resetServerError = () => {
     showError(null)
@@ -41,115 +42,120 @@ const RegisterForm = () => {
   }
 
   const sendDetailsToServer = async () => {
-    resetServerError()
     try {
       const regResp = await userRegister({ email, password })
-
         if(regResp.status === 201){
-          setState(prevState => ({
-            ...prevState,
-            'successMessage' : 'Registration successful. Redirecting to home page..'
-          }))
-          await router.push('/runs-main')
+          await router.push("/runs-main")
+          setLoader(false)
         } else{
           if (regResp.response.status) {
             showError(regResp.response.data.error.message);
           }
         }
     } catch (e) {
-      showError('Sorry, something went wrong')
+      showError("Sorry, something went wrong")
     }
+    setLoader(false)
   }
 
   const handleSubmitClick = async (e) => {
+    resetServerError()
     if(password === confirmPassword) {
+      setLoader(true)
       await sendDetailsToServer(e)
     } else {
-      showError('Passwords do not match');
+      showError("Passwords do not match");
     }
   }
 
   const redirectToLogin = async () => {
-    await router.push('/login');
+    await router.push("/login");
   }
 
   return(
-    <div className='card col-12 col-lg-4 login-card mt-2 hv-center'>
+    <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
       <form onSubmit={handleSubmit(handleSubmitClick)}>
-        <div className='form-group text-left'>
-          <label htmlFor='exampleInputEmail1'>Email address</label>
-          <input className='form-control'
-                 data-testid='email-input'
-                 id='email'
-                 name='email'
-                 aria-describedby='emailHelp'
-                 placeholder='Enter email'
+        <div className="form-group text-left">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input className="form-control"
+                 data-testid="email-input"
+                 id="email"
+                 name="email"
+                 aria-describedby="emailHelp"
+                 placeholder="Enter email"
                  onChange={resetServerError}
                  ref={register}
           />
-          <small id='emailHelp' className='form-text text-muted'>We'll never share your email with anyone else.</small>
-          <div className='alert alert-danger mt-2'
-               style={{display: errors.email ? 'block' : 'none' }}
-               role='alert'>
+          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+          <div className="alert alert-danger mt-2"
+               style={{display: errors.email ? "block" : "none" }}
+               role="alert">
             {errors.email && errors.email.message}
           </div>
         </div>
-        <div className='form-group text-left'>
-          <label htmlFor='exampleInputPassword1'>Password</label>
-          <input type='password'
-                 data-testid='password-input'
-                 className='form-control'
-                 id='password'
-                 name='password'
-                 placeholder='Enter password'
+        <div className="form-group text-left">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password"
+                 data-testid="password-input"
+                 className="form-control"
+                 id="password"
+                 name="password"
+                 placeholder="Enter password"
                  onChange={resetServerError}
                  ref={register}
           />
         </div>
-        <div className='alert alert-danger mt-2'
-             style={{display: errors.password ? 'block' : 'none' }}
-             role='alert'>
+        <div className="alert alert-danger mt-2"
+             style={{display: errors.password ? "block" : "none" }}
+             role="alert">
           {errors.password && errors.password.message}
         </div>
-        <div className='form-group text-left'>
-          <label htmlFor='exampleInputPassword1'>Confirm Password</label>
-          <input type='password'
-                 data-testid='confirmPassword-input'
-                 className='form-control'
-                 id='confirmPassword'
-                 name='confirmPassword'
-                 placeholder='Confirm password'
+        <div className="form-group text-left">
+          <label htmlFor="exampleInputPassword1">Confirm Password</label>
+          <input type="password"
+                 data-testid="confirmPassword-input"
+                 className="form-control"
+                 id="confirmPassword"
+                 name="confirmPassword"
+                 placeholder="Confirm password"
                  onChange={resetServerError}
                  ref={register}
           />
         </div>
-        <div className='alert alert-danger mt-2'
-             style={{display: errors.confirmPassword ? 'block' : 'none' }}
-             role='alert'>
+        <div className="alert alert-danger mt-2"
+             style={{display: errors.confirmPassword ? "block" : "none" }}
+             role="alert">
           {errors.confirmPassword && errors.confirmPassword.message}
         </div>
         <button
-          type='submit'
-          className='btn btn-primary'
-          name='register'
+          type="submit"
+          className="btn btn-primary"
+          name="register"
           disabled={!canContinue}
         >
+          { loader && (
+            <div
+              className="loader"
+              data-testid="loader"
+              style={{ marginLeft: "17px" }}
+            />
+          )}
           Register
         </button>
       </form>
-      <div className='alert alert-success mt-2'
-           style={{display: state.successMessage ? 'block' : 'none' }}
-           role='alert'>
+      <div className="alert alert-success mt-2"
+           style={{display: state.successMessage ? "block" : "none" }}
+           role="alert">
         {state.successMessage}
       </div>
-      <div className='alert alert-danger mt-2'
-           style={{display: state.error ? 'block' : 'none' }}
-           role='alert'>
+      <div className="alert alert-danger mt-2"
+           style={{display: state.error ? "block" : "none" }}
+           role="alert">
         {state.error}
       </div>
-      <div className='mt-2'>
+      <div className="mt-2">
         <span>Already have an account? </span>
-        <span className='loginText' onClick={() => redirectToLogin()}>Login here</span>
+        <span className="loginText" onClick={() => redirectToLogin()}>Login here</span>
       </div>
 
     </div>

60de9fcc786a132ef55e6fa9b47dd46dbb35fcd8
Finlay <FinlayS@users.noreply.github.com>
2021-03-18 20:41:13 +0000

    Account styling (#57)
    * fixing broken styling
    
    * more contrast

--- src/pages/runs-main.jsx
+++ src/pages/runs-main.jsx
@@ -1,14 +1,14 @@
-import React, {useEffect, useReducer} from 'react';
-import { ContextDevTool } from 'react-context-devtool';
-import Container from 'react-bootstrap/Container';
+import React, {useEffect, useReducer} from "react";
+import { ContextDevTool } from "react-context-devtool";
+import Container from "react-bootstrap/Container";
 
-import { getRuns } from '../api/runs';
-import runReducer from '../reducers/runReducers';
-import RunContext from '../context/runContext';
+import { getRuns } from "../api/runs";
+import runReducer from "../reducers/runReducers";
+import RunContext from "../context/runContext";
 
-import RunList from '../components/runs/RunList';
-import Header from '../components/Header';
-import { useRouter } from 'next/router'
+import RunList from "../components/runs/RunList";
+import Header from "../components/Header";
+import { useRouter } from "next/router"
 import lapReducer from "../reducers/lapReducer";
 import LapContext from "../context/lapContext";
 
@@ -25,8 +25,8 @@ const RunsMain = () => {
       try {
         runs = await getRuns()
         if(runs.status === 401) {
-          localStorage.removeItem('token')
-          await router.push('/login')
+          localStorage.removeItem("token")
+          await router.push("/login")
           return
         }
       } catch (e) {
@@ -36,7 +36,7 @@ const RunsMain = () => {
       if (runs) {
         dispatchRuns(
           {
-            type: 'POPULATE_RUNS',
+            type: "POPULATE_RUNS",
             runs
           })
       }
@@ -46,13 +46,12 @@ const RunsMain = () => {
 
   return (
     <RunContext.Provider value={{runs, dispatchRuns}}>
-      <ContextDevTool context={RunContext} id='runs' displayName='Run Context'/>
+      <ContextDevTool context={RunContext} id="runs" displayName="Run Context"/>
       <LapContext.Provider value={{laps, dispatchLaps}}>
-      <ContextDevTool context={LapContext} id='laps' displayName='Lap Context'/>
+      <ContextDevTool context={LapContext} id="laps" displayName="Lap Context"/>
       <Header authed={true}/>
-      {/*<h3>Runs</h3>*/}
-      <div>
-        <Container>
+      <div className="backgroundGrey">
+        <Container >
           <RunList/>
         </Container>
       </div>

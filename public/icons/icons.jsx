172999e93633d90f058887acd3a962bcc14c55a5
Finlay <FinlayS@users.noreply.github.com>
2021-03-27 18:16:01 +0000

    expand style improvemtns (#59)

--- public/icons/icons.jsx
+++ public/icons/icons.jsx
@@ -38,7 +38,8 @@ export const SaveIcon = () => {
 
 export const ExpandIcon = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-chevron-expand btn-bold svg-align">
+    <svg xmlns="http://www.w3.org/2000/svg"
+         className="bi bi-chevron-expand svg-bold-align" viewBox="0 0 16 16">
       <path
             d="M3.646 9.146a.5.5 0 0 1 .708 0L8 12.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708zm0-2.292a.5.5 0 0 0 .708 0L8 3.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708z"/>
     </svg>
@@ -47,7 +48,8 @@ export const ExpandIcon = () => {
 
 export const CollapseIcon = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-chevron-contract svg-align" >
+    <svg xmlns="http://www.w3.org/2000/svg"
+         className="bi bi-chevron-contract svg-bold-align" viewBox="0 0 16 16">
       <path
             d="M3.646 13.854a.5.5 0 0 0 .708 0L8 10.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708zm0-11.708a.5.5 0 0 1 .708 0L8 5.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708z"/>
     </svg>
